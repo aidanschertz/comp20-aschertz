f43309d701ec795452faaa368c843c05ad73487b
Aidan Schertz <aidan.schertz@Aidans-MacBook-Pro-2.local>
2019-03-03 17:56:10 -0500

    adding index.html, lift.js, mapstyle.css, and all pngs used by lift.js

---
+++ liftknockoff/lift.js
@@ -0,0 +1,155 @@
+var mylat = 0;
+var mylng = 0;
+var minDis = 100000000000;
+var me = new google.maps.LatLng(mylat, mylng);
+var info = new google.maps.InfoWindow();
+var usernames =[];
+var pos = [];
+var distances = [];
+var mks = [];
+var type;
+var wstatus;
+var weiner = false;
+var weinIndex;
+var pic;
+var cont;
+var map;
+var marker;
+var content;
+myOptions = {
+    zoom: 10,
+    center: me,
+}
+
+function initMap() {      
+    map = new google.maps.Map(document.getElementById('map'), myOptions);
+    getLocation();
+}
+
+function getLocation() {
+	if (navigator.geolocation) {navigator.geolocation.getCurrentPosition(function(position) {
+		mylat = position.coords.latitude;
+		mylng = position.coords.longitude;
+		createMap();
+	    });
+    }
+    else {
+		alert("Geolocation not supported by browser");
+    }
+}
+
+function createMap() {
+    me = new google.maps.LatLng(mylat, mylng);
+    map.panTo(me);
+    marker = new google.maps.Marker({
+	    position: me,
+	    icon: "peachh.png"
+        });
+    marker.setMap(map);
+    loadRideInfo();
+
+    google.maps.event.addListener(marker, 'click', function (){
+	    info.setContent(content);
+	    info.open(map, marker);
+        });
+}
+    
+function loadRideInfo(){
+    var request = new XMLHttpRequest();
+    var url = 'https://hans-moleman.herokuapp.com/rides';
+    var params = 'orem=ipsum&name=binny';
+    request.open('POST', url, true);
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    request.onreadystatechange = function() {  
+		if(request.readyState == 4 && request.status == 200) {
+        	obj = JSON.parse(request.responseText);
+        	
+            
+          	if (obj.vehicles){
+		    	type = "vehicle"; 
+		    	for (count = 0; count < obj.vehicles.length ; count++){
+				usernames[count] = obj.vehicles[count].username;
+				pos[count] = new google.maps.LatLng(obj.vehicles[count].lat, obj.vehicles[count].lng);
+				distances[count] = google.maps.geometry.spherical.computeDistanceBetween(me,pos[count]) * 0.000621371;
+                
+				if (obj.vehicles[count].username == "WEINERMOBILE"){
+			    	weiner = true;
+			    	weinIndex = count;
+			    	pic = "weinermobile1.png";
+				}
+				else{
+			    	pic = "car1.png";
+				}
+
+				mks[count] = new google.maps.Marker({
+					position: pos[count],
+					icon: pic,
+					data: obj.vehicles[count].username + " is " + distances[count] + " miles away"
+			    	});
+				mks[count].setMap(map);
+				cont = mks[count].data;
+
+				google.maps.event.addListener(mks[count], 'click', function (){
+					info.setContent(cont);
+					info.open(map, marker);
+			    	});
+		    	}
+
+		    	if (weiner == true){
+					wstatus = "The Weinermobile is " + distances[weinIndex] + " miles away from me!";
+		    	} 
+		    	else{
+					wstatus = "The Weinermobile is nowhere to be seen";
+		    	}
+			}
+
+            if (obj.passengers){
+		    	type = "passenger";
+		    	for (count = 0; count < obj.passengers.length ; count++){
+					usernames[count] = obj.passengers[count].username;
+					pos[count] = new google.maps.LatLng(obj.passengers[count].lat, obj.passengers[count].lng);
+					distances[count] = google.maps.geometry.spherical.computeDistanceBetween(me,pos[count]) * 0.000621371;
+                	
+                	if (obj.passengers[count].username == "WEINERMOBILE"){
+			    		weiner = true;
+			    		weinIndex = count;
+			    		pic = "weinermobile1.png";
+					}
+					else{
+			    		pic = "cherry.png";
+					}
+
+					mks[count] = new google.maps.Marker({
+						position: pos[count],
+						icon: pic,
+						data: obj.passengers[count].username + " is " + distances[count] + " miles away"
+			    	});
+					mks[count].setMap(map);
+
+					google.maps.event.addListener(mks[count], 'click', function (){
+						info.setContent(mks[count].data);
+						info.open(map, marker);
+			    	});
+		    	}
+
+		    	if (weiner == true){
+					wstatus = "The Weinermobile is " + distances[weinIndex] + " miles away from me!";
+		    	} 
+		    	else{
+					wstatus = "The Weinermobile is nowhere to be seen";
+		    	}  
+			}
+
+            for (count = 0; count < pos.length ; count++){
+				if (distances[count] < minDis){
+		    		minDis = distances[count];
+				}		
+            }
+            content = "User R5zer572:<br> The closest " + type + " is " + minDis + " miles away<br>" + wstatus;
+        }
+    }
+    request.send("username=R5zer572&lat="+mylat+"&lng="+mylng);
+};
+
+ 
